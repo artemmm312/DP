7d7bf22fd556155de67ee2917cca637735378703
artemmm312 <artemmm312@gmail.com>
2022-06-01 18:11:31 +0300

    fix header

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,20 @@
-window.onload = () => {
+jQuery(function () {
+  $(".bottom-header__nav-menu-link").hover(function () {
+    $(this).toggleClass("line-height");
+  });
+  $(".drop").on("mouseenter", () => $('.shadow').show());
+  $(".drop").on("mouseleave", () => $('.shadow').hide());
+  $(".dropdown").on("mouseenter", () => $('.shadow').show());
+  $(".dropdown").on("mouseleave", () => $('.shadow').hide());
+  $("#burger").on('change', function () {
+    if (this.checked) {
+      $('.shadow').show();
+    } else {
+      $('.shadow').hide();
+    }
+  });
+
+
   var options = {
     year: 'numeric',
     month: 'long',
@@ -7,4 +23,4 @@ window.onload = () => {
   };
   let data = new Date().toLocaleDateString('ru', options);
   document.getElementById('header-date').innerHTML = data[0].toUpperCase() + data.substring(1);
-};
+});
